ac51a49a7d85934d8dd8c9233fdc50d2b111279c
Yasmin Saraya Leripio <yasminsaraya16@gmail.com>
2026-02-28 20:08:15 -0300

    Configurei tema claro e tema escuro

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,11 @@ const temasalvo = localStorage.getItem('tema');
 temaEscuro(temasalvo === 'escuro');
 
 // Função para alternar entre tema claro e escuro
-function temaEscuro(tipo) {
-  if (tipo == true) {
-    body.classList.add('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+function temaEscuro(ativo) {
+  if (ativo) {
+    botao.textContent = 'Claro';
   } else {
-    body.classList.remove('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    botao.textContent = 'Escuro';
   }
 }
 
